dd416c31fa675e7fbeefac7f9b6ddf23d32c3d6b
Andrei <andrei.bast@yahoo.com>
2020-05-13 17:00:26 +0300

    layoutFirstTrial

--- index.php
+++ index.php
@@ -16,61 +16,17 @@
 <body>
 
     <div>
-        <img class="img-responsive" src="modules/pictures/topsitebanner.jpg">
+        <img id="banner" class="img-responsive" src="modules/pictures/topsitebanner.jpg">
     </div>
     
     <div>
         <?php include("modules/topbar.php"); ?> 
         <?php include("modules/carousel.php"); ?> 
-        <div class="push-left">
-            <?php include("modules/sidebar.php"); ?> 
-        </div>
-        <div class="">
         <?php include("modules/center.php"); ?> 
-        </div>
-        <div class="">
         <?php include("modules/footer.php"); ?> 
-        </div>
-        
-        
-        
+       
 
-        <!-- <div class="row">
-            
-                <div class="col-md-2">
-                
-                
-                </div>
-                <div class="col-md-1">
-
-                </div>
-                <div class="col-md-8">
-                
-                </div>
-            
-
-        </div> -->
-
-
-        
-        
-        
-        
     </div>
-
-    
-    
-    
-            
-
-
-
-
-            
-
-
-
-    
 
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
--- modules/center.php
+++ modules/center.php
@@ -1,23 +1,165 @@
 
 <div class="container-main-center">
 
+    <!-------------------- 
+    1.FILTERS 
+    ---------------------->
+    <div class="zone green">
 
-    <div class="zone green">🦊
+        <!-- 
+        1.1.SEARCH SECTION 
+        -->
+        <div>
+            <aside class="col-md-3">
+                <div class="row">
+                    <output>
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="input-group">
+                                    <input type="text" placeholder="Keyword" class="form-control" name="">
+                                    <span class="input-group-append"> <button class="btn btn-primary"> <i class="fa fa-search"></i></button></span>
+                                </div>
+                            </div>
+                        </div> <!-- card.// -->
+                    </output>
+                </div>
+            </aside> 
+        </div>
+
+      
+    <!-- 1.2.TAGS SECTION -->
+    <div>
+        <aside class="col-md-3">
+        <div class="row">
+            <output>
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Tags</h5>
+                        <label class="custom-control custom-checkbox">
+                            <input type="checkbox" checked="" class="custom-control-input">
+                            <div class="custom-control-label"><h6>#architecture  <b class="badge badge-pill badge-light float-right">120</b></h6>  </div>
+                        </label>
+
+                        <label class="custom-control custom-checkbox">
+                            <input type="checkbox" checked="" class="custom-control-input">
+                            <div class="custom-control-label"><h6>#b&w <b class="badge badge-pill badge-light float-right">15</b></h6>  </div>
+                        </label>
+
+                        <label class="custom-control custom-checkbox">
+                            <input type="checkbox" checked="" class="custom-control-input">
+                            <div class="custom-control-label"><h6>#emotion <b class="badge badge-pill badge-light float-right">34</b></h6>  </div>
+                        </label>
+
+                        <label class="custom-control custom-checkbox">
+                            <input type="checkbox" checked="" class="custom-control-input">
+                            <div class="custom-control-label"><h6>#landscape <b class="badge badge-pill badge-light float-right">76</b></h6>  </div>
+                        </label>
+
+                        <label class="custom-control custom-checkbox">
+                            <input type="checkbox" checked="" class="custom-control-input">
+                            <div class="custom-control-label"><h6>#naturebeauty <b class="badge badge-pill badge-light float-right">89</b></h6>  </div>
+                        </label>
+                    </div>
+                </div> <!-- card.// -->
+            </output>
+            </div>
+        </aside> 
+        </div>
+    
+
+    <!-- 1.3.RATINGS SECTION -->
+    <div>
+        <aside class="col-md-3">
+        <div class="row">
+            <output>
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Pictures Rating</h5>
+                        <label class="custom-control custom-checkbox">
+                        <input type="checkbox" checked="" class="custom-control-input">
+                        <div class="custom-control-label text-warning"> 
+                            <h6><i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> 
+                            <i class="fa fa-star"></i> <i class="fa fa-star"></i> </h6>
+                        </div>
+                        </label>
+
+                        <label class="custom-control custom-checkbox">
+                        <input type="checkbox" checked="" class="custom-control-input">
+                        <div class="custom-control-label text-warning"> 
+                            <h6><i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> 
+                            <i class="fa fa-star"></i> </h6>
+                        </div>
+                        </label>
+
+                        <label class="custom-control custom-checkbox">
+                        <input type="checkbox" checked="" class="custom-control-input">
+                        <div class="custom-control-label text-warning"> 
+                            <h6><i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> 
+                            </h6>
+                        </div>
+                        </label>
+
+                        <label class="custom-control custom-checkbox">
+                        <input type="checkbox" checked="" class="custom-control-input">
+                        <div class="custom-control-label text-warning"> 
+                            <h6><i class="fa fa-star"></i> <i class="fa fa-star"></i>  </h6>
+                        </div>
+                        </label>
+
+                        <label class="custom-control custom-checkbox">
+                        <input type="checkbox" checked="" class="custom-control-input">
+                        <div class="custom-control-label text-warning"> 
+                            <h6><i class="fa fa-star"></i> </h6>
+                        </div>
+                        </label>
+                    </div>
+                </div> <!-- card.// -->
+            </output>
+            </div>
+        </aside> 
+        </div>
     </div>
 
-  <div class="zone red">
-    <img class ="picture-center" src="modules/pictures/center/20190714_190747.jpg" alt="">
+   
+
+
+
+    <!-------------------- 
+    2.IMAGES SECTION
+    ---------------------->
+    <div class=" red container-main-center2">
+        <div class="zone blue container-main-center3">
+            <img class ="picture-center container-main-center4 img-block" src="modules/pictures/center/20190714_190749.jpg" alt="">
+        </div>
+
+        <div class="zone yellow container-main-center3">
+            <img class ="picture-center container-main-center4 img-block" src="modules/pictures/center/20190714_190758.jpg" alt="">
+        </div>
+
+        <div class="zone purple container-main-center3">
+            <img class ="picture-center container-main-center4 img-block" src="modules/pictures/center/20190714_191701.jpg" alt="">
+        </div>
+
+        <div class="zone blue container-main-center3">
+            <img class ="picture-center container-main-center4 img-block" src="modules/pictures/center/20190714_190749.jpg" alt="">
+        </div>
+
+        <div class="zone yellow container-main-center3">
+            <img class ="picture-center container-main-center4 img-block" src="modules/pictures/center/20190714_190758.jpg" alt="">
+        </div>
+
+        <div class="zone purple container-main-center3">
+            <img class ="picture-center container-main-center4 img-block" src="modules/pictures/center/20190714_191701.jpg" alt="">
+        </div>
+
+        
+
+       
+
+    </div>
+
+  
   </div>
 
-  <div class="zone blue">
-    <img class ="picture-center" src="modules/pictures/center/20190714_190749.jpg" alt="">
-  </div>
-
-  <div class="zone yellow">
-    <img class ="picture-center" src="modules/pictures/center/20190714_190758.jpg" alt="">
-  </div>
-
-  <div class="zone purple">
-    <img class ="picture-center" src="modules/pictures/center/20190714_191701.jpg" alt="">
-  </div>
-  </div>
+  <script src="js/jquery-3.5.0.js"></script>
+<script src="js/bootstrap.min.js"></script>
